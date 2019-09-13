2d4dd42fdf7b976bdee0096377dc5e92aba52d77
dbdevelop <dbdevelop8@gmail.com>
2019-09-13 21:59:25 +0700

    first commit

---
+++ app/Http/Controllers/developcontroller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class developcontroller extends Controller
+{
+
+
+
+	public function admin()
+
+
+
+		{
+
+
+
+
+
+			return view('admin.index');
+
+
+
+
+
+
+		}
+    
+
+
+}
